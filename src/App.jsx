76677ff08b77a4cea0b48e4ad1714ba3686b0663
amanyadav12designer <YOUR_GITHUB_EMAIL>
2026-04-08 14:24:47 +0530

    final fixed

--- src/App.jsx
+++ src/App.jsx
@@ -133,7 +133,7 @@ async function toggleFavorite(id) {
 
     setProperties(prev =>
       prev.map(p =>
-        p.id === id ? { ...p,favorite: normalized.favorite } : p
+        p.id === id ? { ...p,favorite: !normalized.favorite } : p
       )
     );
 
